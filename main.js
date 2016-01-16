bda56302eaf99c40aa2c4ac7d57f7f59b9e64710
William B <william@barkoffusa.com>
2016-01-16 18:12:45 -0500

    Ugh, installer broken

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-function clickOrigin(e){
-    var target = e.target;
-    var tag = [];
-    tag.tagType = target.tagName.toLowerCase();
-    tag.tagClass = target.className.split(' ');
-    tag.id = target.id;
-    tag.parent = target.parentNode;
-
-    return tag;
+if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+  document.getElementById("download").innerHTML = "Sorry, this screensaver is limited to OSX.";
 }
-
-var tagsToIdentify = ['a'];
-
-document.body.onclick = function(e){
-    elem = clickOrigin(e);
-      if (elem.tagType == tagsToIdentify[i]){
-          document.getElementById("download").innerHTML = "When the download is complete, unzip the installer and run it."
-          return false; // or do something else.
-    }
-};
